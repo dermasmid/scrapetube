1868f17649a219317905594f466aa3dcef55358d
Cheskel Twersky <twerskycheskel@gmail.com>
2021-08-20 00:23:22 +0300

    add documentation link

--- setup.py
+++ setup.py
@@ -21,13 +21,16 @@ setup(
     long_description_content_type = 'text/markdown',
     author = 'Cheskel Twersky',
     author_email = 'twerskycheskel@gmail.com',
-    description = 'Scrape youtube without the official youtube api and without selenium',
+    description = 'Scrape youtube without the official youtube api and without selenium.',
     keywords = 'youtube python channel videos search playlist list get',
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    project_urls={
+        'Documentation': 'https://scrapetube.readthedocs.io/en/latest/'
+    },
     install_requires = requirements,
     python_requires = '>=3.6',
 )

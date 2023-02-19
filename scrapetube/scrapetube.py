77de7e0bdab3d3580be6eca1c40d8cf5228a4c97
VladislavSmolyanoy <vlad_smolyanoy@icloud.com>
2023-02-19 19:22:37 +0100

    bruh forgor to add it actually

--- scrapetube/scrapetube.py
+++ scrapetube/scrapetube.py
@@ -154,6 +154,7 @@ def get_videos(
     session.headers[
         "User-Agent"
     ] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"
+    session.headers["Accept-Language"] = "en"
     is_first = True
     quit = False
     count = 0

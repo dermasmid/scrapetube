e3bf7ddbd1c8b4be106a4606e6d9ad1a2a5e9e7b
twissell- <dmaggioesne@gmail.com>
2023-09-03 14:14:05 +0200

    Replaced the SOCS cookie for ucbcb request param

--- scrapetube/scrapetube.py
+++ scrapetube/scrapetube.py
@@ -241,8 +241,7 @@ def get_session() -> requests.Session:
 
 def get_initial_data(session: requests.Session, url: str) -> str:
     session.cookies.set("CONSENT", "YES+cb", domain=".youtube.com")
-    session.cookies.set("SOCS", "CAESEwgDEgk0ODE3Nzk3MjQaAmVuIAEaBgiA_LyaBg", domain=".youtube.com")
-    response = session.get(url)
+    response = session.get(url, params={"ucbcb":1})
 
     html = response.text
     return html
